67876caa532475da71a9cfd1a6782b4846215c4c
Pierr Dunn <ihavemyselfinbafroom@gmail.com>
2018-03-13 23:27:20 +0300

    !

---
+++ app/src/main/java/com/pierrdunn/locatr/FlickrFetchr.java
@@ -0,0 +1,158 @@
+package com.pierrdunn.locatr;
+
+import android.graphics.LinearGradient;
+import android.location.Location;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by pierrdunn on 09.03.18.
+ * Сетевая поддержка (Flickr)
+ */
+
+public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+
+    private static final String API_KEY = "e2ddc6feeca22e9c12f7fe8b0a401617";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s,geo")
+            .build();
+
+
+    //Получает низкоуровневые данные по URL и
+    //возвращает их в виде массива байтов
+    public byte[] getUrlBytes(String urlSpec) throws IOException{
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpsURLConnection) url
+                .openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode() != HttpsURLConnection.HTTP_OK)
+                throw new IOException(connection.getResponseMessage()
+                + ": with " + urlSpec);
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0)
+                out.write(buffer, 0, bytesRead);
+
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    //Преобразует getUrlBytes в String
+    public String getUrlString(String urlSpec) throws IOException{
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    /*
+    * методы для получения последних фотографий и поиска
+    * */
+    public List<GalleryItem> fetchRecentPhotos(){
+        String url = buildUrl(FETCH_RECENTS_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query){
+        String url = buildUrl(FETCH_RECENTS_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(Location location){
+        String url = buildUrl(location);
+        return downloadGalleryItems(url);
+    }
+
+    //Построение URL запроса
+    private List<GalleryItem> downloadGalleryItems(String url){
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            //разбор json ответа
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        } catch (IOException ioe){
+            Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (JSONException je){
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+
+        return items;
+    }
+
+    //Построитель запроса
+    private String buildUrl(String method, String query){
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+        if (method.equals(SEARCH_METHOD))
+            uriBuilder.appendQueryParameter("text", query);
+        return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Location location){
+        return  ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
+    }
+
+    //Разбор распарщенных фотографий
+    private void parseItems(List<GalleryItem> items, JSONObject jsonObject)
+        throws IOException, JSONException{
+
+        JSONObject photosJsonObject = jsonObject.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++){
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s"))
+                continue;
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
+            item.setLat(photoJsonObject.getDouble("latitude"));
+            item.setLat(photoJsonObject.getDouble("longitude"));
+            Log.i(TAG, "OGGGGGGGGGGGGO"
+                    + photoJsonObject.getDouble("latitude")
+                    + photoJsonObject.getDouble("longitude"));
+            items.add(item);
+        }
+
+    }
+}
